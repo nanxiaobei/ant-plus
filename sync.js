d2cda832a09c3fb3e141226f33004a9b80d5f77a
nanxiaobei <nanxiaobei@gmail.com>
2019-09-10 15:47:36 +0800

    📲 Add sync.sh

---
+++ sync.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+
+const headerData = `---
+name: 快速开始
+route: /
+---
+
+import './theme.scss';
+
+# 快速开始
+
+## 介绍
+`;
+
+const licenseText = '## 协议';
+
+const gitHubText = `## GitHub
+
+[https://github.com/nanxiaobei/ant-plus](https://github.com/nanxiaobei/ant-plus)
+
+`;
+
+function syncReadmeToDoczIndex() {
+  const readme = fs.readFileSync('./README.md', 'utf-8');
+
+  let contentData = readme.replace(/.+\s/, '');
+  contentData = contentData.replace(/(\[!\[npm.+)|(\[!\[GitHub.+)/g, '');
+  contentData = contentData.replace(/\s{5}/g, '');
+
+  const licenseIndex = contentData.indexOf(licenseText);
+  contentData = contentData.slice(0, licenseIndex) + gitHubText + contentData.slice(licenseIndex);
+
+  const data = headerData + contentData;
+
+  fs.writeFileSync('./docz/Index.mdx', data, 'utf-8');
+}
+
+syncReadmeToDoczIndex();
